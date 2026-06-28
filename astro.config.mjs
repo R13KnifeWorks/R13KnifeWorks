3b9ae624276e4d0105b1aea5f731e212621991cc
Jacob Atnip <69258012+JakeAtnip@users.noreply.github.com>
2026-06-28 17:37:14 -0500

    Update Astro config for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://r13knifeworks.github.io',
-  base: '/R13KnifeWorks/',
+  site: 'https://r13knifeworks.com',
 });
